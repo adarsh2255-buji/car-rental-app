0baeedb4dc5ffb0b3734feb6ec46549b19cfdde8
adarsh <adarshsomanasrt@gmail.com>
2024-08-12 22:55:08 +0530

    signin functionality added

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/esm/Container';
+import { useNavigate } from 'react-router-dom';
+import api from '../../api';
 
 const SignIn = () => {
+  const navigate = useNavigate();
+ 
+  const [ signInData, setSignInData ] = useState({
+    email : "",
+    password : "",
+  })
+
+  const handleChange = (e) =>{
+    setSignInData({...signInData, [e.target.name] : e.target.value})
+      
+  }
+
+  const submitHandler = async(e) =>{
+    e.preventDefault();
+    try {
+      const response = await api.post('/signin', signInData);
+      navigate('/home')
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+ 
   return (
-    <div>SignIn</div>
+    <>
+    <h1 className='text-center mt-5'>LOG IN HERE</h1>
+    <Container className='d-flex justify-content-center align-items-center'>
+    <Form onSubmit={submitHandler}>
+     
+      <Form.Group className="mb-3">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="text" placeholder="email"
+        name='email'
+        value={signInData.email}
+        onChange={handleChange}
+        required/>
+      </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="password" 
+        name='password'
+        value={signInData.password}
+        onChange={handleChange}
+        required />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </Container>
+    </>
   )
 }
 
