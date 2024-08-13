54395a4badcdb6de754a36f30cce6acda0224c37
adarsh <adarshsomanasrt@gmail.com>
2024-08-13 19:53:17 +0530

    styled the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,19 +5,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import { LinkContainer} from 'react-router-bootstrap'
 import { UserContext } from '../context/UserContext';
 
-
-
-
 const Header = () => {
   const { user, logout } = useContext(UserContext)
   return (
     <>
-     <Navbar expand="lg" className="bg-body-tertiary">
+    <Container><p className='text-end'>{user ? user.username : "Guest"}</p></Container>
+     <Navbar expand="lg" className="bg-success bg-gradient">
+    
       <Container>
-        <Navbar.Brand href="#home">Car rental</Navbar.Brand>
+        <Navbar.Brand href="#home" className='text-white fs-2 fw-bold'>Wheels Hub</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             {user ? (
               <>
               <LinkContainer to='/home'><Nav.Link >Home</Nav.Link></LinkContainer>
@@ -25,7 +24,7 @@ const Header = () => {
               <LinkContainer to='/contact'><Nav.Link >Contact</Nav.Link></LinkContainer>
               <LinkContainer to='/signout'><Nav.Link onClick={logout}>Sign out</Nav.Link></LinkContainer>
               </>
-            ): (
+            ) : (
               <>
                 <LinkContainer to='/home'><Nav.Link >Home</Nav.Link></LinkContainer> 
                 <LinkContainer to='/about'><Nav.Link >About</Nav.Link></LinkContainer> 
@@ -38,7 +37,6 @@ const Header = () => {
                       
           </Nav>
         </Navbar.Collapse>
-        <p>{user ? user.username : "Guest"}</p>
       </Container>
       
     </Navbar>
