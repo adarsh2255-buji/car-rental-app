709b9438a5a45522fc61663b1f1e07f11c03cf47
adarsh <adarshsomanasrt@gmail.com>
2024-08-14 14:22:51 +0530

    user token verification error fixed

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -5,25 +5,46 @@ import User from "../model/userModel.js";
 const protect = async(req, res, next) =>{
     let token;
 
-    if(req.cookies.token){
+    // if(req.cookies.token){
+    //     try {
+    //         //get token from cookie
+    //         token = req.cookies.token;
+
+    //         //verify token
+    //         const decode = jwt.verify(token, process.env.JWT_SECRET)
+
+    //         //get user from token
+    //         req.user = await User.findById(decode.userId).select('-password');
+    //         next()
+    //     } catch (error) {
+    //         console.log(error);
+    //         res.status(401).json({ message : "not autherized, token failed" });
+    //     }
+    // }
+
+    // if(!token) {
+    //     res.status(401).json({ message : "Not authorized, no token" })
+    // }
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            //get token from cookie
-            token = req.cookies.token;
+            // Get token from the header
+            token = req.headers.authorization.split(' ')[1];
+            console.log(token)
 
-            //verify token
-            const decode = jwt.verify(token, process.env.JWT_SECRET)
+            // Verify token
+            const decode = jwt.verify(token, process.env.JWT_SECRET);
 
-            //get user from token
+            // Get user from token
             req.user = await User.findById(decode.userId).select('-password');
-            next()
+            next();
         } catch (error) {
             console.log(error);
-            res.status(401).json({ message : "not autherized, token failed" });
+            res.status(401).json({ message: "Not authorized, token failed" });
         }
     }
 
-    if(!token) {
-        res.status(401).json({ message : "Not authorized, no token" })
+    if (!token) {
+        res.status(401).json({ message: "Not authorized, no token" });
     }
 };
 
@@ -36,4 +57,4 @@ export const adminMiddleware = (req, res, next) => {
         res.status(403).json({ message : "Access denied"})
     }
 }
-export default protect;
+export default protect; 
