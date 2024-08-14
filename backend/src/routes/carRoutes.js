14a5768f7692b29a2d659d5f1eb7556bc0de7181
adarsh <adarshsomanasrt@gmail.com>
2024-08-14 14:23:47 +0530

    get all car routes added

--- backend/src/routes/carRoutes.js
+++ backend/src/routes/carRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import upload from '../config/cloudinary.js';
-import { createCar, getCarById } from '../controllers/carController.js';
+import { createCar, getAllCars, getCarById } from '../controllers/carController.js';
 
 const router = express.Router();
 
 router.post('/createCar',createCar);
 router.get('/getCar/:id',getCarById);
+router.get('/cars',getAllCars);
 
 
 
