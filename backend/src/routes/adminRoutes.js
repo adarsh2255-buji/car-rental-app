a3b1c989f77b9a98f3b9e40b923cfdaae6c92c0a
adarsh <adarshsomanasrt@gmail.com>
2024-08-23 13:27:55 +0530

    updated create car for upload image

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import protect, { adminMiddleware } from "../middlewares/authMiddleware.js";
 import { carAvailability, getAllBookings, getAllUsers } from "../controllers/adminContoller.js";
+import { createCar, uploadCarImage } from "../controllers/carController.js";
 
 const router = express.Router()
 
 router.get('/getAllUsers',protect, adminMiddleware, getAllUsers);
 router.get('/getAllBooking',protect, adminMiddleware, getAllBookings);
 router.put('/car/:carId/availability',protect, adminMiddleware, carAvailability);
+router.post('/createCar',uploadCarImage, protect, adminMiddleware, createCar);
 export default router;
