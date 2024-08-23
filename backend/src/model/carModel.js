463f32fa2d3eaa1f81e332e5fff33549dfa6996d
adarsh <adarshsomanasrt@gmail.com>
2024-08-23 13:29:04 +0530

    allowedkm updated

--- backend/src/model/carModel.js
+++ backend/src/model/carModel.js
@@ -5,7 +5,6 @@ const carSchema = mongoose.Schema({
     model : {type : String, required : true},
     fuelType : {type : String, required : true},
     gearTransmission : {type : String, required : true},
-    allowedKM : {type : Number, required : true},
     kmPerDay : {type : Number, required : true},
     seater : {type : Number, required : true},
     image : {type : String, required : true},
