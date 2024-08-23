d47f5b163a3cf8a7eac7c6cd4154738a041e69cf
adarsh <adarshsomanasrt@gmail.com>
2024-08-23 13:31:48 +0530

    no commit

--- backend/src/routes/carRoutes.js
+++ backend/src/routes/carRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import upload from '../config/cloudinary.js';
-import { createCar, getAllCars, getCarById } from '../controllers/carController.js';
+import { getAllCars, getCarById } from '../controllers/carController.js';
 
 const router = express.Router();
 
-router.post('/createCar',createCar);
+
 router.get('/getCar/:id',getCarById);
 router.get('/cars',getAllCars);
 
