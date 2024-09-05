eb3ba5695fad62664c81c8a08875ac15fc7c1b2f
adarsh <adarshsomanasrt@gmail.com>
2024-09-05 14:13:47 +0530

    delete car route added

--- backend/src/routes/carRoutes.js
+++ backend/src/routes/carRoutes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 // import upload from '../config/cloudinary.js';
-import { getAllCars, getCarById } from '../controllers/carController.js';
+import { deleteCarById, getAllCars, getCarById } from '../controllers/carController.js';
 
 const router = express.Router();
 
 
 router.get('/getCar/:id',getCarById);
 router.get('/cars',getAllCars);
+router.delete('/deleteCar/:id',deleteCarById)
 
 
 
